41e11f1b5eea683e30c9d8433c20514cf41ceb4b
danie <danie@DESKTOP-BP5DVML>
2022-12-16 10:53:03 +0100

    version1

---
+++ aaaa/src/aaaa/Aaaa.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aaaa;
+
+/**
+ *
+ * @author danie
+ */
+public class Aaaa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       ...for (String arg : args) {
+            
+        }
+    }
+    
+}
